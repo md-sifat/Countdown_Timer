9fa210cd973564de73565f196e4169922b53aca8
mdsifat <mdsifat7252@gmail.com>
2023-01-31 20:16:16 +0600

    finised up

--- js/app.js
+++ js/app.js
@@ -6,19 +6,24 @@ const clock = () =>{
     const end = new Date(endDate);
     const start = new Date();
     const diff = (end - start) /1000;
-    console.log(diff);
+
     const days =Math.floor(diff/3600/24);
     const hour =Math.floor((diff/3600)%24);
     const min =Math.floor((diff/60)%60);
     const sec =Math.floor(diff%60);
+
     inputs[0].value = days;
     inputs[1].value = hour;
     inputs[2].value = min;
     inputs[3].value = sec;
-    console.log(days);
-    console.log(hour);
-    console.log(min);
-    console.log(sec);
+
 }
 
+
+setInterval(
+    () => {
+        clock();
+    },
+    1000
+)
 clock();
