db35db02641281d2cf11a77f85dc6b3945b490d3
mdsifat <mdsifat7252@gmail.com>
2023-01-31 20:12:26 +0600

    timer added

--- js/app.js
+++ js/app.js
@@ -1,11 +1,24 @@
-const endDate = "21 February 2023 10:00 PM";
+const endDate = "21 February 2023 11:57 PM";
 
 document.getElementById("endDate").innerHTML = endDate;
+const inputs =  document.querySelectorAll("input");
 const clock = () =>{
     const end = new Date(endDate);
     const start = new Date();
-    const diff = (end - start) / 1000;
+    const diff = (end - start) /1000;
     console.log(diff);
+    const days =Math.floor(diff/3600/24);
+    const hour =Math.floor((diff/3600)%24);
+    const min =Math.floor((diff/60)%60);
+    const sec =Math.floor(diff%60);
+    inputs[0].value = days;
+    inputs[1].value = hour;
+    inputs[2].value = min;
+    inputs[3].value = sec;
+    console.log(days);
+    console.log(hour);
+    console.log(min);
+    console.log(sec);
 }
 
 clock();
